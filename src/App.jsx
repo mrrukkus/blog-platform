94b88a1dd8362732f57d0df7ace4e915de12c4f6
mrrukkus <doooog1@rambler.ru>
2020-11-16 16:56:25 +0500

    Создана разметка + примерные стили под шапку с кнопками регистрации/авторизации

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="app-header">
+        <div className="header-wrapper">
+          <h1 className="app-header__header">Realworld blog</h1>
+          <div className="auth-buttons">
+            <button className="auth-buttons__button">Sign In</button>
+            <button className="auth-buttons__button auth-buttons__button--auth">Sign Up</button>
+          </div>
+        </div>
+      </header>
+      {/* компонент */}
+    </>
+  );
+};
+
+export default App;
